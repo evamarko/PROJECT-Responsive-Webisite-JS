033337e15578f65b820bbd15a5990d62e791dbe8
Eva Marko <valentova.eva92@gmail.com>
2024-09-30 11:58:21 -0600

    Dark and light mode completed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //Responsive menu
 const menuIcon = document.querySelector(".menu-icon");
 const menuList = document.querySelector("nav");
-const hamburgerIcon = document.querySelector(".fa-solid");
+const hamburgerIcon = document.querySelector(".hamburger");
 
 menuIcon.addEventListener("click", () => {
-  if (hamburgerIcon.classList[1] === "fa-bars") {
+  if (hamburgerIcon.classList[2] === "fa-bars") {
     hamburgerIcon.classList.add("fa-xmark");
     hamburgerIcon.classList.remove("fa-bars");
     menuList.style.display = "block";
@@ -74,4 +74,38 @@ window.addEventListener("scroll", () => {
 
 arrowUp.addEventListener("click", () => {
   window.scrollTo(0, 0);
+});
+
+//Dark and ligh mode
+const modeIcon = document.querySelector(".mode-icon");
+const moonIcon = document.querySelector(".mode");
+const body = document.querySelector("body");
+const vipsection = document.querySelector(".vipsection");
+const h2 = document.querySelector("h2");
+const h3 = document.querySelector("h3");
+const h2vipsection = document.querySelector(".vipsection h2");
+const formContainer = document.querySelector(".form-container");
+
+modeIcon.addEventListener("click", () => {
+  if (moonIcon.classList[2] === "fa-moon") {
+    moonIcon.classList.add("fa-sun");
+    moonIcon.classList.remove("fa-moon");
+    body.style.backgroundColor = "#282425";
+    body.style.color = "#fff1e6";
+    vipsection.style.backgroundColor = "#474747";
+    h2.style.color = "#fff1e6";
+    h2vipsection.style.color = "#fff1e6";
+    h3.style.color = "#fff1e6";
+    formContainer.style.backgroundColor = "#282425";
+  } else {
+    moonIcon.classList.add("fa-moon");
+    moonIcon.classList.remove("fa-sun");
+    body.style.backgroundColor = "#fff1e6";
+    body.style.color = "#282425";
+    vipsection.style.backgroundColor = "#f9e3d0";
+    h2.style.color = "#756158";
+    h2vipsection.style.color = "#756158";
+    h3.style.color = "#756158";
+    formContainer.style.backgroundColor = "#fff";
+  }
 });
